3f6ea2c324cb94678eb9dc8a9cf99985d3b92d62
jeffrey <2059428@talnet.nl>
2020-09-21 12:56:28 +0200

    placeholder weggehaald

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
 <body>
 	<form action="" method="POST">
 		<label><b>Username</b></label>
-		<input type="text" placeholder="Enter Username" name="username" required><br/>
+		<input type="text" name="username" required><br/>
 
 		<label><b>Password</b></label>
-		<input type="password" placeholder="Enter Password" name="password" required><br/>
+		<input type="password" name="password" required><br/>
 
 		<input type="submit" value="Login"><br/>
 
